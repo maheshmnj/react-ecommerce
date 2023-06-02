c8e877a0fd2beaad608fa9051f483098e7e099d5
mahesh jamdade <maheshmn121@gmail.com>
2023-06-02 08:16:31 -0400

    render a element inside root

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -9,9 +8,12 @@ if (rootElement === null) {
   throw new Error("Root element not found");
 } else {
   const root = createRoot(rootElement);
+  const element = React.createElement('h1', {
+    className: 'title',
+  },
+    'Online Supermarket'
+  )
   root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
+    element
   );
 }
