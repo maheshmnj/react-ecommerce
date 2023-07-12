fb6f0e43f20ca2a1a7885d285941c194fe02a016
mahesh jamdade <maheshmn121@gmail.com>
2023-07-11 23:05:10 -0400

    add icons

--- src/components/Common/Sidebar.tsx
+++ src/components/Common/Sidebar.tsx
@@ -1,22 +1,40 @@
-import { Drawer } from "@mui/material";
+import { Drawer, IconButton } from "@mui/material";
 import React, { useState } from "react";
 import "./common.css";
+import ArrowBackIosIcon from '@mui/material/IconButton/';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faChevronLeft, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+
+const MenuTitle = (props: any) => {
+  return (
+    <div className="list-title" onClick={props.onMenuClick}>
+      <div className="menu-icon">
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </div>
+      <div className="list-title-title">
+        <h3>{props.title}</h3>
+      </div>
+    </div>
+  );
+
+};
+
+
 const Sidebar = (props) => {
   return (
     <>
       {props.isSidebarOpen && (
         <div className="drawer-content">
           <div>
-            <button onClick={props.onSidebarClose}>
-              Close Sidebar
-            </button>
+            <IconButton aria-label="Example"
+              onClick={props.onSidebarClose}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </IconButton>
           </div>
           <ul>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
+            <MenuTitle title="Menu" onMenuClick={() => console.log("clicked")} />
+            <MenuTitle title="Menu" />
+            <MenuTitle title="Menu" />
           </ul>
         </div>
       )}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,12 +3,18 @@ import "../Navbar/navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { BeakerIcon } from "@heroicons/react/24/solid";
+import IconButton from "@mui/material/IconButton";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 export default function Navbar(props: any) {
   return (
     <>
       <div className="frost navbar">
         <div className="navbar-menu">
-          <button onClick={props.onSidebarToggle}>Toggle Sidebar</button>
+          {/* hamburger icon */}
+          <IconButton aria-label="Example"
+            onClick={props.onSidebarToggle}>
+            <FontAwesomeIcon icon={faBars} />
+          </IconButton>
           <li>
             {/* <BeakerIcon className="h-6 w-6 text-blue-500" /> */}
             <Logo title="Todoist" />
